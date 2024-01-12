8133218174fab07d5add8b516567c174a151ed40
mikhailvasilev <mall.87@mail.ru>
2024-01-12 11:09:23 +0300

    some refactor100500

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -17,47 +17,11 @@ public abstract class BaseSchema {
     }
 
     public final boolean isValid(Object object) {
-
         if (object == null) {
             return !isRequired;
         }
         return conditions.values().stream().allMatch(n -> n.test(object));
     }
-//        if (!isRequired) {
-//            return true;
-//        }
-//        for (Map.Entry<String, Predicate<Object>> condition : conditions.entrySet()) {
-//            if (!condition.getValue().test(object)) {
-//                isRequired = false;
-//                break;
-//            }
-//        }
-
-//        for (Map.Entry<String, Predicate<Object>> condition : conditions.entrySet()) {
-//            if (!condition.getValue().test(object)) {
-//                return false;
-//            }
-//        }
-
-//        if (!conditions.values().stream()
-//                .allMatch(predicate -> predicate.test(object))) {
-//            return false;
-//        }
-
-//        boolean requiredPredicate = conditions.get("isRequired").test(object);
-//        if (!isRequired && requiredPredicate) {
-//            return true;
-//        }
-//        for (Map.Entry<String, Predicate<Object>> condition : conditions.entrySet()) {
-//            if (!conditions.values().stream()
-//                .allMatch(predicate -> predicate.test(object))) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//        return true;
-
 
     protected final void addCondition(String name, Predicate<Object> condition) {
         conditions.put(name, condition);
